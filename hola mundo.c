5796fdca11d1e2acfbb6b1d4fa237441b6dc5bff
Itseak <keren.hernandez5518@alumnos.udg.mx>
2025-10-21 18:04:37 -0600

    Add files via upload

---
+++ hola mundo.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+int main()
+{
+    int mes;
+ printf("===Selecci%cn multiple, estaciones del a%co ===\n",162,164);
+
+ printf("Para conocer en que estacion del a%co estas, ingresa el mes actual: ",164);
+ scanf("%d",&mes);
+
+ switch(mes)
+ {
+ case 1:
+
+
+ case 2:
+    printf("\n>> Tu estacion es invierno\n");
+    break;
+ case 3:
+
+ case 4:
+
+ case 5:
+     printf("\n>> Tu estacion es primavera\n");
+     break;
+ case 6:
+
+
+ case 7:
+     case 8:
+         printf("\n>> Tu estacion es verano\n");
+         break;
+     case 9:
+     case 10:
+     case 11:
+         printf("\n>> Tu estacion es oto%c\n",164);
+         break;
+     case 12:
+         printf("\n>> Tu estacion es invierno\n");
+        break;
+
+
+
+
+
+
+
+
+ }
+
+
+
+
+
+
+
+ return 0;
+}
